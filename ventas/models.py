3395e9ef1d8917295440c4bf3c063d4e5e1fe7f3
reiner <reinerjj11@gmail.com>
2025-10-07 00:08:20 -0500

    models

--- ventas/models.py
+++ ventas/models.py
@@ -1,17 +1,17 @@
 from django.db import models
 from django.contrib.auth.models import User
 from clientes.models import Cliente
+from cloudinary.models import CloudinaryField
 
 class Producto(models.Model):
     nombre = models.CharField(max_length=100)
     precio = models.DecimalField(max_digits=10, decimal_places=2)
     stock = models.PositiveIntegerField()
     categoria = models.CharField(max_length=50, blank=True, null=True)
-    imagen = models.ImageField(upload_to='productos/', null=True, blank=True)
-    
+    imagen = CloudinaryField('image', blank=True, null=True)
+
     def __str__(self):
         return self.nombre
-
 
 class Venta(models.Model):
     fecha = models.DateTimeField(auto_now_add=True)
